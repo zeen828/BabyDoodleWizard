a7a41345a20a4f9bc3376e07899a6c2b3322925e
Will <zeren828@gmail.com>
2021-02-18 12:14:31 +0800

    VUE畫布動態測試

--- src/components/capture/Apple.vue
+++ src/components/capture/Apple.vue
@@ -1,67 +1,58 @@
 <template>
-  <canvas id="canvas" ref="canvas" class="bg-info">蘋果</canvas>
+  <canvas id="canvasApple" ref="canvas" class="bg-info" width="400" height="400">蘋果</canvas>
 </template>
 
 <script>
-function getOffset (Node, offset) {
-  if (!offset) {
-    offset = {}
-    offset.top = 0
-    offset.left = 0
-  }
-  if (Node === document.body) {
-    return offset
-  }
-  offset.top += Node.offsetTop
-  offset.left += Node.offsetLeft
-  return getOffset(Node.parentNode, offset)
-}
 export default {
   name: 'Apple',
   methods: {
     ready () {
       // 畫蘋果
-      // this.canvas = document.querySelector('#captureApple')
-      // this.ctx = this.canvas.getContext('2d')
+      this.initCanvas()
+      // this.setApple()
+      setInterval(this.drawCanvas, 32)
     },
     initCanvas () {
-      this.canvas = document.getElementById('captureApple')
-      this.canvas.width = this.canvasSize.width
-      this.canvas.height = this.canvasSize.height
+      this.canvas = document.getElementById('canvasApple')
       this.context = this.canvas.getContext('2d')
-      this.canvasTop = getOffset(this.canvas).top - 108
-      this.canvasLeft = getOffset(this.canvas).left - (135 / 915) * this.scrW
     },
-    apple () {
-      // 抓Dom&宣告2D
-      const canvas = document.querySelector('#captureApple')
-      const ctx = canvas.getContext('2d')
-      // 畫蘋果(紅圓)
-      ctx.fillStyle = 'red'
-      ctx.beginPath()
-      ctx.arc(30, 30, 20, 0, Math.PI * 2)
-      ctx.fill()
-      ctx.closePath()
-      ctx.beginPath()
-      ctx.rotate(20 * Math.PI / 180)
-      ctx.arc(40, 10, 8, 0, Math.PI)
-      ctx.strokeStyle = '#000000'
-      ctx.stroke()
+    setApple () {
+      this.context.fillStyle = 'red'
+      this.context.beginPath()
+      this.context.arc(30, 30, 20, 0, Math.PI * 2)
+      this.context.fill()
+      this.context.closePath()
+      this.context.beginPath()
+      this.context.rotate(20 * Math.PI / 180)
+      this.context.arc(40, 10, 8, 0, Math.PI)
+      this.context.strokeStyle = '#000000'
+      this.context.stroke()
+    },
+    drawCanvas () {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      this.context.beginPath()
+      this.context.arc(this.x, this.y, 12, 0, Math.PI * 2, true)
+      this.context.closePath()
+      this.context.fill()
+      this.x += 6
+      if (this.x > 560) this.x = 10
     }
   },
   data () {
     return {
       // 運作狀態
       isRunning: false,
-      canvasSize: {
-        width: window.screen.availWidth * 0.8,
-        height: window.screen.availHeight * 0.75
-      },
+      // canvasSize: {
+      //   width: window.screen.availWidth * 0.8,
+      //   height: window.screen.availHeight * 0.75
+      // },
       // 畫布
       canvas: this.$refs.canvas,
-      canvasTop: null,
-      canvasLeft: null,
+      // canvasTop: null,
+      // canvasLeft: null,
       context: null,
+      x: 30,
+      y: 30,
       config: {}
     }
   },
